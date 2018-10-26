e524474489733aba53f253d1688fd2389bdcf24b
Kerry Ivan Kurian <kkurian@gmail.com>
2018-10-26 03:06:44 -0700

    Remove cruft

--- teleportal.js
+++ teleportal.js
@@ -1,37 +1,12 @@
 'use strict';
 /* jslint vars:true, plusplus:true, forin:true */
-/* global Script, Promise, print */
+/* global Script, print */
 (function () { // BEGIN LOCAL SCOPE
-    var AppUi = Script.require('appUi');
+    // var AppUi = Script.require('appUi');
     var request = Script.require('request').request;
 
-    /// XXX AWS
-    // var AWS = Script.require(Script.resolvePath('aws-sdk-2.343.0.min.js'));
-    // AWS.config.update({region: 'us-west-2'});
-    // AWS.config.credentials = new AWS.credentials(
-    //   'AKIAIPF6MCR6FSNAUB7Q',
-    //   'Gep5lTKJB5K/5+8ureMP5zMFnYq8k2W5YC1yKbZu'
-    // );
-    // var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-    ///
-
-    function onOpened() {
-        /// XXX AWS
-        // var params = {
-        //   TableName: "hello_world",
-        //   Item: {
-        //     'id': {N: 1}
-        //   }
-        // };
-        // ddb.putItem(params, function (err, data) {
-        //   if (err) {
-        //     console.log("Error", err);
-        //   } else {
-        //     console.log("Success", data);
-        //   }
-        // });
-        ///
-    }
+    // function onOpened() {
+    // }
 
     function sheetsuHandleResponse(retry) {
         return function(err, response) {
